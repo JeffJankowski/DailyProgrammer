81af4792cffcb9b8c8931b560ee345970baa5e51
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2015-06-15 18:34:12 -0700

    Added #219 Easy
    To-do list written in F#

---
+++ 219 Easy - ToDo/Program.fs
@@ -0,0 +1,23 @@
+﻿[<EntryPoint>]
+let main argv = 
+
+
+    let addItem item list = 
+        item :: list
+    let deleteItem item list = 
+        List.filter (fun i -> i <> item) list
+    let printList list = 
+        printf "TODO:\n"
+        list |> List.rev |> List.iter (fun i -> printf "  - %s\n" i)
+
+    List.Empty
+    |> addItem "Buy food, or starve to death whatever.."
+    |> addItem "Get better at F#"
+    |> addItem "Probably just go to bed"
+    |> deleteItem "Get better at F#"
+    |> printList
+
+
+    System.Console.ReadKey() |> ignore
+    0
+
